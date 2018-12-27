20b032a9c2e8ca8b3fd42ca9178ec0f10b388f40
alaadah95 <alaadah_331@yahoo.com>
2018-12-28 00:05:41 +0200

    Navigation Drawer

--- app/src/main/java/com/alaa/microprocess/lrahtk/Fragment/SignIn.java
+++ app/src/main/java/com/alaa/microprocess/lrahtk/Fragment/SignIn.java
@@ -1,23 +1,26 @@
 package com.alaa.microprocess.lrahtk.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.alaa.microprocess.lrahtk.Contract.MainActivityContract;
 import com.alaa.microprocess.lrahtk.R;
+import com.alaa.microprocess.lrahtk.View.HomePage;
 
 public class SignIn extends Fragment implements View.OnClickListener{
 
     TextView skiptoSignUP;
     MainActivityContract.View mainView;
     ImageView skiptosignupimage;
-
+    Button button;
 
     @Nullable
     @Override
@@ -26,8 +29,21 @@ public class SignIn extends Fragment implements View.OnClickListener{
 
         skiptoSignUP = view.findViewById(R.id.skiptoSignUP);
         skiptosignupimage = view.findViewById(R.id.skiptosignupimage);
+        button            = view.findViewById(R.id.email_sign_in_button);
         skiptosignupimage.setOnClickListener(this);
         skiptoSignUP.setOnClickListener(this);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+                Intent intent = new Intent(getActivity() , HomePage.class);
+                startActivity(intent);
+
+
+            }
+        });
+
         return view;
     }
 
--- app/src/main/java/com/alaa/microprocess/lrahtk/View/HomePage.java
+++ app/src/main/java/com/alaa/microprocess/lrahtk/View/HomePage.java
@@ -2,16 +2,79 @@ package com.alaa.microprocess.lrahtk.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
 import com.alaa.microprocess.lrahtk.R;
+import com.special.ResideMenu.ResideMenu;
+import com.special.ResideMenu.ResideMenuItem;
 
-public class HomePage extends AppCompatActivity {
+
+public class HomePage extends AppCompatActivity implements View.OnClickListener {
+    ResideMenu  resideMenu;
+
+    ResideMenuItem home_item;
+    ResideMenuItem setting_item;
+    ResideMenuItem signout_item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-        
+        // attach to current activity;
+        resideMenu = new ResideMenu(this);
+        resideMenu.setBackground(R.color.blue);
+        resideMenu.attachToActivity(this);
+
+
+
+
+             home_item = new ResideMenuItem(this, R.drawable.user, "Home");
+             setting_item = new ResideMenuItem(this, R.drawable.user, "Setting");
+             signout_item = new ResideMenuItem(this, R.drawable.user, "Sign Out");
+
+             resideMenu.addMenuItem(home_item,  ResideMenu.DIRECTION_LEFT); // or  ResideMenu.DIRECTION_RIGHT
+             resideMenu.addMenuItem(setting_item,  ResideMenu.DIRECTION_LEFT); // or  ResideMenu.DIRECTION_RIGHT
+             resideMenu.addMenuItem(signout_item,  ResideMenu.DIRECTION_LEFT); // or  ResideMenu.DIRECTION_RIGHT
+
+
+        home_item.setOnClickListener(this);
+        setting_item.setOnClickListener(this);
+        signout_item.setOnClickListener(this);
+
+        resideMenu.openMenu(ResideMenu.DIRECTION_LEFT); // or ResideMenu.DIRECTION_RIGHT
+        resideMenu.setSwipeDirectionDisable(ResideMenu.DIRECTION_RIGHT);
+
+
+
+
     }
-}
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        return resideMenu.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view == home_item ){
+            Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+        }
+
+        else if (view == setting_item ) {
+
+            Toast.makeText(this, "Setting", Toast.LENGTH_SHORT).show();
+        }
+        else if (view == signout_item ){
+            Toast.makeText(this, "sign out", Toast.LENGTH_SHORT).show();
+          }
+
+        }
+
+    }
+
+
+
