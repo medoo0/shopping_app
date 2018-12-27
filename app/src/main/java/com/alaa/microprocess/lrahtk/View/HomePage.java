7a565506b3a28c18b020bf33f42666b8cf85605c
medoo0 <raslanmohamed156@yahoo.com>
2018-12-27 12:39:29 -0800

    skip betwwen fragments finished

---
+++ app/src/main/java/com/alaa/microprocess/lrahtk/View/HomePage.java
@@ -0,0 +1,17 @@
+package com.alaa.microprocess.lrahtk.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.alaa.microprocess.lrahtk.R;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        
+    }
+}
