e176f0b097f0d729eae0153f64c6c0b8b724e1f0
alaadah95 <alaadah_331@yahoo.com>
2018-12-29 17:19:30 +0200

    strings .

--- app/src/main/java/com/alaa/microprocess/lrahtk/Fragment/SignUp.java
+++ app/src/main/java/com/alaa/microprocess/lrahtk/Fragment/SignUp.java
@@ -38,17 +38,18 @@ public class SignUp extends Fragment implements View.OnClickListener{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.signup_layout,container,false);
-        skip   = v.findViewById(R.id.skip);
+        View v            = inflater.inflate(R.layout.signup_layout,container,false);
+        skip              = v.findViewById(R.id.skip);
         skiptoSignInimage = v.findViewById(R.id.skiptoSignInimage);
         register_btn      = v.findViewById(R.id.register_btn);
-        UserName  = v.findViewById(R.id.UserName);
-        Email     = v.findViewById(R.id.Email);
-        MobilNumber = v.findViewById(R.id.MobilNumber);
-        password    = v.findViewById(R.id.password);
+        UserName          = v.findViewById(R.id.UserName);
+        Email             = v.findViewById(R.id.Email);
+        MobilNumber       = v.findViewById(R.id.MobilNumber);
+        password          = v.findViewById(R.id.password);
+
+
         register_btn.setOnClickListener(this);
         skiptoSignInimage.setOnClickListener(this);
-
         skip.setOnClickListener(this);
 
 
@@ -87,41 +88,89 @@ public class SignUp extends Fragment implements View.OnClickListener{
 
         }
 
-        if(view == register_btn){
+        if(view == register_btn) {
 
             ApiMethod client = ApiRetrofit.getRetrofit().create(ApiMethod.class);
+
             RegisterForm registerForm = new RegisterForm();
             registerForm.setName(UserName.getText().toString());
             registerForm.setPhone(MobilNumber.getText().toString());
             registerForm.setEmail(Email.getText().toString());
             registerForm.setPassword(password.getText().toString());
 
-            Call<RegisterResponse> call = client.insertData(registerForm.getCreatedAt(),registerForm.getUpdatedAt(),registerForm.getId()
-            ,registerForm.getEmail(),registerForm.getPassword(),registerForm.getName(),registerForm.getPhone());
+            if(UserName.getText().toString().isEmpty()){
 
-            call.enqueue(new Callback<RegisterResponse>() {
-                @Override
-                public void onResponse(@NonNull Call<RegisterResponse> call, @NonNull Response<RegisterResponse> response) {
-                 if (response.isSuccess())
-                 {
-                     Toast.makeText(getActivity(), "Tmam", Toast.LENGTH_SHORT).show();
+                UserName.setError(R.string.Name_error);
+                UserName.requestFocus();
 
-                 }
-                 else {
+            }
+            else if (Email.getText().toString().isEmpty()){
+                Email.setError(R.string.email_error);
+                Email.requestFocus();
+            }
+            else if(isEmailValid(Email.getText().toString())){
+                Email.setError(R.string.email_error2);
+                Email.requestFocus();
+            }
+            else if(password.getText().toString().isEmpty()){
 
-                     Toast.makeText(getActivity(), "no tmam", Toast.LENGTH_SHORT).show();
-                 }
+                password.setError(R.string.password_error);
+                password.requestFocus();
+            }
+            else if (password.getText().toString().length() < 6 ){
+
+                password.setError(R.string.password_error2);
+                password.requestFocus();
+            }
+            else {
+
+                Register(client,registerForm.getCreatedAt(), registerForm.getUpdatedAt(), registerForm.getId()
+                        , registerForm.getEmail(), registerForm.getPassword(), registerForm.getName(), registerForm.getPhone());
+
+            }
 
 
-                }
 
-                @Override
-                public void onFailure(@NonNull Call<RegisterResponse> call, @NonNull Throwable t) {
-                    Toast.makeText(getActivity(), "not registered", Toast.LENGTH_SHORT).show();
-                }
-            });
 
         }
 
     }
+
+
+public void Register(ApiMethod client ,String createdAt, String updatedAt, String id, String email, String password, String name, String phone) {
+
+    Call<RegisterResponse> call = client.insertData(createdAt,updatedAt,id
+            ,email,password, name, phone);
+
+    call.enqueue(new Callback<RegisterResponse>() {
+        @Override
+        public void onResponse(@NonNull Call<RegisterResponse> call, @NonNull Response<RegisterResponse> response) {
+            if (response.isSuccess()) {
+                //lOGIN
+                Toast.makeText(getActivity(), "Tmama", Toast.LENGTH_SHORT).show();
+
+            } else {
+
+                Toast.makeText(getActivity(), "no tmam", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
+
+        @Override
+        public void onFailure(@NonNull Call<RegisterResponse> call, @NonNull Throwable t) {
+            Toast.makeText(getActivity(), "not registered", Toast.LENGTH_SHORT).show();
+        }
+    });
+
+
+}
+
+
+
+    boolean isEmailValid(CharSequence email) {
+
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches(); //returen false if not ok //return true if ok
+    }
+
 }
--- app/src/main/java/com/alaa/microprocess/lrahtk/pojo/Errors.java
+++ app/src/main/java/com/alaa/microprocess/lrahtk/pojo/Errors.java
@@ -4,5 +4,6 @@ package com.alaa.microprocess.lrahtk.pojo;
  * Created by microprocess on 2018-12-29.
  */
 
-class Errors {
+ public class Errors {
+
 }
